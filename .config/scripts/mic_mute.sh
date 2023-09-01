c3e93451c0fcac7d61c4898c6222da6edc95b1b1
stardustgd <88991140+stardustgd@users.noreply.github.com>
2023-08-31 18:20:18 -0700

    Update mic_mute.sh

--- .config/scripts/mic_mute.sh
+++ .config/scripts/mic_mute.sh
@@ -7,7 +7,7 @@ notification_timeout=3000
 notification_id=9990
 
 if [ $STATUS == "yes" ]; then 
-    notify-send "Microphone" "Muted" -i ~/.local/share/icons/microphone-off.png -t $notification_timeout -r $notification_id 
+    notify-send "Microphone" -i ~/.local/share/icons/microphone-off.png -t $notification_timeout -r $notification_id 
 else 
-    notify-send "Microphone" "Unmuted" -i ~/.local/share/icons/microphone.png -t $notification_timeout -r $notification_id
+    notify-send "Microphone" -i ~/.local/share/icons/microphone.png -t $notification_timeout -r $notification_id
 fi
