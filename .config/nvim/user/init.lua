f27d574f48ce72d5ff6242c81ea28f6810c5e185
stardustgd <88991140+stardustgd@users.noreply.github.com>
2024-01-23 13:18:03 -0800

    Update init.lua

--- .config/nvim/user/init.lua
+++ .config/nvim/user/init.lua
@@ -12,6 +12,13 @@ return {
     -- easily add new remotes to track
     remotes = {},
   },
+  mappings = {
+    n = {
+      -- Recenter when page up/down
+      ["<C-d>"] = {"<C-d>zz", desc = "Move page down"},
+      ["<C-u>"] = {"<C-u>zz", desc = "Move page up"},
+    },
+  },
   -- Set colorscheme to use
   colorscheme = "nord",
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
@@ -24,7 +31,7 @@ return {
     formatting = {
       -- control auto formatting on save
       format_on_save = {
-        enabled = true, -- enable or disable format on save globally
+        enabled = false, -- enable or disable format on save globally
         -- enable format on save for specified filetypes only
         allow_filetypes = {},
         -- disable format on save for specified filetypes
@@ -48,4 +55,56 @@ return {
   },
   polish = function()
   end,
+    plugins = {
+    {
+      "rebelot/heirline.nvim",
+      opts = function(_, opts)
+        local status = require "astronvim.utils.status"
+
+        opts.winbar = { -- create custom winbar
+          -- store the current buffer number
+          init = function(self) self.bufnr = vim.api.nvim_get_current_buf() end,
+          fallthrough = false, -- pick the correct winbar based on condition
+          -- inactive winbar
+          {
+            condition = function() return not status.condition.is_active() end,
+            -- show the path to the file relative to the working directory
+            status.component.separated_path { path_func = status.provider.filename { modify = ":.:h" } },
+            -- add the file name and icon
+            status.component.file_info {
+              file_icon = { hl = status.hl.file_icon "winbar", padding = { left = 0 } },
+              file_modified = false,
+              file_read_only = false,
+              hl = status.hl.get_attributes("winbarnc", true),
+              surround = false,
+              update = "BufEnter",
+            },
+          },
+          -- active winbar
+          {
+            -- show the path to the file relative to the working directory
+            status.component.separated_path { path_func = status.provider.filename { modify = ":.:h" } },
+            -- add the file name and icon
+            status.component.file_info { -- add file_info to breadcrumbs
+              file_icon = { hl = status.hl.filetype_color, padding = { left = 0 } },
+              file_modified = false,
+              file_read_only = false,
+              hl = status.hl.get_attributes("winbar", true),
+              surround = false,
+              update = "BufEnter",
+            },
+            -- show the breadcrumbs
+            status.component.breadcrumbs {
+              icon = { hl = true },
+              hl = status.hl.get_attributes("winbar", true),
+              prefix = true,
+              padding = { left = 0 },
+            },
+          },
+        }
+
+        return opts
+      end,
+    },
+  },
 }
